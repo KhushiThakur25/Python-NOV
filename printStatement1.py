4759719ba17576fae83afbe30148beb028e8b508
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-18 10:09:05 +0530

    Add files via upload

---
+++ printStatement1.py
@@ -0,0 +1,40 @@
+'''print("""ksdjfhjd
+    dfgfhf
+dfgf
+    fghfhgjfggfgh
+dfghfhj""")'''
+#for single line comment we use hash symbol
+'''And for multi line comment we use triple
+single inverted comma's'''
+'''a = 6
+b = 7
+c = a+b
+d = a/b
+
+print(c)
+print("Sum is",c)
+print("Sum is",c,sep="----")
+print("Sum of %d and %d is %d"%(a,c,b))
+print("Sum is {}".format(c))
+print("Sum of {0} and {2} is {1}".format(a,c,b))
+
+# f-string
+print(f"div of {a} and {b} is {d :.3f}")'''
+
+
+#walrus operator:
+a = 6
+b = 7
+
+print(a+b)
+
+print(z := a+b)
+print(f"Sum of {a} and {b} is {(z := a+b)}")
+
+
+
+
+
+
+
+
