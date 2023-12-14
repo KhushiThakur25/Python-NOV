16a6485293d7792da0e3381f497c512f649f71c9
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-14 10:03:38 +0530

    Add files via upload

---
+++ ListsClass1.py
@@ -0,0 +1,5 @@
+# to print even values of lists
+lists = [65,23,41,78,41,91,12,60]
+for i in lists:
+    if i%2 == 0:
+        print(i)
---
+++ ListsClass2.py
@@ -0,0 +1,12 @@
+'''lists = [1,2,3,4,5]
+sums = 0
+for i in lists:
+    sums += i
+print(sums)'''
+lists = [1,2,3,4,5]
+lists_2 = []
+for i in range(len(lists)):
+    lists_2.append(lists[i]*2)
+print(lists_2)
+print(lists)
+    
